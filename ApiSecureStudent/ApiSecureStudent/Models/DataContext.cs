7eed46358300026714d25cfbf51976262ca14ab1
OscarNLA <a2019114151@estudiantes.upsa.edu.bo>
2020-09-05 02:41:40 -0400

    version 1

---
+++ ApiSecureStudent/ApiSecureStudent/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Data.Entity;
+
+namespace ApiSecureStudent.Models
+{
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ApiSecureStudent.Models.Student> Students { get; set; }
+    }
+}
