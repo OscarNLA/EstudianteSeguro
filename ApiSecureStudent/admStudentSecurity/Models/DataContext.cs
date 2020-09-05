2ab37c0a72568b43c295e91bfec9dc16aeafd03e
OscarNLA <a2019114151@estudiantes.upsa.edu.bo>
2020-09-05 11:56:23 -0400

    se agrego el proyecto mvc en administracion

---
+++ ApiSecureStudent/admStudentSecurity/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ApiSecureStudent.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<admStudentSecurity.Models.Student> Students { get; set; }
+    }
+}
