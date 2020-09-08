23ba2a390e2de89453efb981a52ef4c94f30f4be
OscarNLA <a2019114151@estudiantes.upsa.edu.bo>
2020-09-08 12:22:17 -0400

    arreglado xd

--- ApiSecureStudent/UtEstudianteSeguro.Test/UnitTest1.cs
+++ ApiSecureStudent/UtEstudianteSeguro.Test/UnitTest1.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Web.Http;
-using ApiSecureStudent.Controllers;
+﻿using ApiSecureStudent.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UtEstudianteSeguro.Test
